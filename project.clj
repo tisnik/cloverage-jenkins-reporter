5629556a8f5a60db66774b89d28ba2605239f8e6
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-09-04 15:27:54 +0200

    Added all Leiningen plugins used by the project

--- project.clj
+++ project.clj
@@ -8,6 +8,9 @@
                    [org.clojure/tools.cli "0.3.1"]
                    [hiccup "1.0.4"]]
     :main ^:skip-aot cloverage-jenkins-reporter.core
+    :plugins [[codox "0.8.11"]
+              [test2junit "1.1.0"]
+              [lein-cloverage "1.0.2"]]
     :target-path "target/%s"
     :profiles {:uberjar {:aot :all}})
 
