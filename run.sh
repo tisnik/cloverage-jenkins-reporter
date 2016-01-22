48dc7aba91bb3705d6bc3406be5fa076d530c254
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-01-22 16:26:48 +0100

    Updated run.sh to generate proper output

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-lein run
+lein run > middle.html
+cat html/header.html > CodeCoverage.html
+cat middle.html      >> CodeCoverage.html
+cat html/footer.html >> CodeCoverage.html
+rm middle.html
 
