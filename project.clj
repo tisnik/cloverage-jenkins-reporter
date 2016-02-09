1b00dcc830244a2ac6682ccece392fe304b4994b
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-02-09 15:23:26 +0100

    Bump up version for the library used by the reporter

--- project.clj
+++ project.clj
@@ -39,7 +39,7 @@
     :main ^:skip-aot cloverage-jenkins-reporter.core
     :plugins [[codox "0.8.11"]
               [test2junit "1.1.0"]
-              [lein-cloverage "1.0.2"]]
+              [lein-cloverage "1.0.6"]]
     :target-path "target/%s"
     :profiles {:uberjar {:aot :all}})
 
