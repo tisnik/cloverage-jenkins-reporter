57c2409d8b051e34345efc1ef5069d95790733fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-02-09 15:31:43 +0100

    Added one test for the core module

--- test/cloverage_jenkins_reporter/core_test.clj
+++ test/cloverage_jenkins_reporter/core_test.clj
@@ -1,7 +1,7 @@
 ;;;
 ;;;   Statistic about code coverage measured by Cloverage reporter.
 ;;;
-;;; Copyright (c) 2015  Pavel Tisnovsky, Red Hat
+;;; Copyright (c) 2015, 2016  Pavel Tisnovsky, Red Hat
 ;;; All rights reserved.
 ;;;
 ;;; Redistribution and use in source and binary forms, with or without
@@ -30,6 +30,21 @@
   (:require [clojure.test :refer :all]
             [cloverage-jenkins-reporter.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for various defs and functions
+;
+
+(deftest test-main-existence
+    "Check that the clouseau.core/-main definition exists."
+    (testing "if the clouseau.core/-main definition exists."
+        (is (callable? 'clouseau.core/-main))))
+
