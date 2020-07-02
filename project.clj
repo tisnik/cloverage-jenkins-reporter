87c3b6d8a659836c53a0c8b173585fd759a222cd
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-02 08:15:08 +0200

    Reformatted project.clj by zprint

--- project.clj
+++ project.clj
@@ -27,19 +27,19 @@
 ;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (defproject cloverage-jenkins-reporter "0.1.0-SNAPSHOT"
-    :description "Reports basic statistic about code coverage measured by Cloverage."
-    :url "http://example.com/FIXME"
-    :license {:name "BSD 3-Clause license"
-              :url "http://opensource.org/licenses/BSD-3-Clause"}
-    :dependencies [[org.clojure/clojure "1.8.0"]
-                   [org.clojure/data.json "0.2.5"]
-                   [org.clojure/tools.cli "0.3.1"]
-                   [clojure-ini "0.0.1"]
-                   [hiccup "1.0.4"]]
-    :main ^:skip-aot cloverage-jenkins-reporter.core
-    :plugins [[codox "0.8.11"]
-              [test2junit "1.1.0"]
-              [lein-cloverage "1.0.6"]]
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}})
+  :description "Reports basic statistic about code coverage measured by Cloverage."
+  :url "http://example.com/FIXME"
+  :license {:name "BSD 3-Clause license",
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [clojure-ini "0.0.1"]
+                 [hiccup "1.0.4"]]
+  :main ^:skip-aot cloverage-jenkins-reporter.core
+  :plugins [[codox "0.8.11"]
+            [test2junit "1.1.0"]
+            [lein-cloverage "1.0.6"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
 
