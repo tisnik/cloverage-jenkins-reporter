e521dcb269d790155df9eddbbcb78b86213b0bd1
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-02 08:44:41 +0200

    Reformatted jenkins_api_test.clj by zprint

--- test/cloverage_jenkins_reporter/jenkins_api_test.clj
+++ test/cloverage_jenkins_reporter/jenkins_api_test.clj
@@ -35,120 +35,138 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for various defs and functions
 ;
 
 (deftest test-list-of-all-jobs-url-existence
-    "Check that the cloverage-jenkins-reporter.jenkins-api/list-of-all-jobs-url definition exists."
-    (testing "if the cloverage-jenkins-reporter.jenkins-api/list-of-all-jobs-url definition exists."
-        (is (callable? 'cloverage-jenkins-reporter.jenkins-api/list-of-all-jobs-url))))
+  "Check that the cloverage-jenkins-reporter.jenkins-api/list-of-all-jobs-url definition exists."
+  (testing
+    "if the cloverage-jenkins-reporter.jenkins-api/list-of-all-jobs-url definition exists."
+    (is (callable?
+          'cloverage-jenkins-reporter.jenkins-api/list-of-all-jobs-url))))
 
 
 (deftest test-read-list-of-all-jobs-existence
-    "Check that the cloverage-jenkins-reporter.jenkins-api/read-list-of-all-jobs definition exists."
-    (testing "if the cloverage-jenkins-reporter.jenkins-api/read-list-of-all-jobs definition exists."
-        (is (callable? 'cloverage-jenkins-reporter.jenkins-api/read-list-of-all-jobs))))
+  "Check that the cloverage-jenkins-reporter.jenkins-api/read-list-of-all-jobs definition exists."
+  (testing
+    "if the cloverage-jenkins-reporter.jenkins-api/read-list-of-all-jobs definition exists."
+    (is (callable?
+          'cloverage-jenkins-reporter.jenkins-api/read-list-of-all-jobs))))
 
 
 (deftest test-read-job-info-as-json-existence
-    "Check that the cloverage-jenkins-reporter.jenkins-api/read-job-info-as-json definition exists."
-    (testing "if the cloverage-jenkins-reporter.jenkins-api/read-job-info-as-json definition exists."
-        (is (callable? 'cloverage-jenkins-reporter.jenkins-api/read-job-info-as-json))))
+  "Check that the cloverage-jenkins-reporter.jenkins-api/read-job-info-as-json definition exists."
+  (testing
+    "if the cloverage-jenkins-reporter.jenkins-api/read-job-info-as-json definition exists."
+    (is (callable?
+          'cloverage-jenkins-reporter.jenkins-api/read-job-info-as-json))))
 
 
 (deftest test-read-build-number-for-job-existence
-    "Check that the cloverage-jenkins-reporter.jenkins-api/read-build-number-for-job definition exists."
-    (testing "if the cloverage-jenkins-reporter.jenkins-api/read-build-number-for-job definition exists."
-        (is (callable? 'cloverage-jenkins-reporter.jenkins-api/read-build-number-for-job))))
+  "Check that the cloverage-jenkins-reporter.jenkins-api/read-build-number-for-job definition exists."
+  (testing
+    "if the cloverage-jenkins-reporter.jenkins-api/read-build-number-for-job definition exists."
+    (is (callable?
+          'cloverage-jenkins-reporter.jenkins-api/read-build-number-for-job))))
 
 
 (deftest test-encode-spaces-existence
-    "Check that the cloverage-jenkins-reporter.jenkins-api/encode-spaces definition exists."
-    (testing "if the cloverage-jenkins-reporter.jenkins-api/encode-spaces definition exists."
-        (is (callable? 'cloverage-jenkins-reporter.jenkins-api/encode-spaces))))
+  "Check that the cloverage-jenkins-reporter.jenkins-api/encode-spaces definition exists."
+  (testing
+    "if the cloverage-jenkins-reporter.jenkins-api/encode-spaces definition exists."
+    (is (callable? 'cloverage-jenkins-reporter.jenkins-api/encode-spaces))))
 
 
 (deftest test-url-to-file-from-last-build-existence
-    "Check that the cloverage-jenkins-reporter.jenkins-api/url-to-file-from-last-build definition exists."
-    (testing "if the cloverage-jenkins-reporter.jenkins-api/url-to-file-from-last-build definition exists."
-        (is (callable? 'cloverage-jenkins-reporter.jenkins-api/url-to-file-from-last-build))))
+  "Check that the cloverage-jenkins-reporter.jenkins-api/url-to-file-from-last-build definition exists."
+  (testing
+    "if the cloverage-jenkins-reporter.jenkins-api/url-to-file-from-last-build definition exists."
+    (is
+      (callable?
+        'cloverage-jenkins-reporter.jenkins-api/url-to-file-from-last-build))))
 
 
 (deftest test-url-to-job-configuration-existence
-    "Check that the cloverage-jenkins-reporter.jenkins-api/url-to-job-configuration definition exists."
-    (testing "if the cloverage-jenkins-reporter.jenkins-api/url-to-job-configuration definition exists."
-        (is (callable? 'cloverage-jenkins-reporter.jenkins-api/url-to-job-configuration))))
+  "Check that the cloverage-jenkins-reporter.jenkins-api/url-to-job-configuration definition exists."
+  (testing
+    "if the cloverage-jenkins-reporter.jenkins-api/url-to-job-configuration definition exists."
+    (is (callable?
+          'cloverage-jenkins-reporter.jenkins-api/url-to-job-configuration))))
 
 
 (deftest test-read-file-from-last-build-existence
-    "Check that the cloverage-jenkins-reporter.jenkins-api/read-file-from-last-build definition exists."
-    (testing "if the cloverage-jenkins-reporter.jenkins-api/read-file-from-last-build definition exists."
-        (is (callable? 'cloverage-jenkins-reporter.jenkins-api/read-file-from-last-build))))
+  "Check that the cloverage-jenkins-reporter.jenkins-api/read-file-from-last-build definition exists."
+  (testing
+    "if the cloverage-jenkins-reporter.jenkins-api/read-file-from-last-build definition exists."
+    (is (callable?
+          'cloverage-jenkins-reporter.jenkins-api/read-file-from-last-build))))
 
 
 (deftest test-read-job-configuration-existence
-    "Check that the cloverage-jenkins-reporter.jenkins-api/read-job-configuration definition exists."
-    (testing "if the cloverage-jenkins-reporter.jenkins-api/read-job-configuration definition exists."
-        (is (callable? 'cloverage-jenkins-reporter.jenkins-api/read-job-configuration))))
+  "Check that the cloverage-jenkins-reporter.jenkins-api/read-job-configuration definition exists."
+  (testing
+    "if the cloverage-jenkins-reporter.jenkins-api/read-job-configuration definition exists."
+    (is (callable?
+          'cloverage-jenkins-reporter.jenkins-api/read-job-configuration))))
 
 ;
 ; Actual tests
 ;
 
 (deftest test-encode-spaces
-    "Check the jenkins-api/encode-spaces function"
-    (testing "the jenkins-api/encode-spaces function"
-        (are [x y] (= x y)
-            ""            (encode-spaces "")
-            "%20"         (encode-spaces " ")
-            "%20%20"      (encode-spaces "  ")
-            "%20%20%20"   (encode-spaces "   ")
-            "x%20"        (encode-spaces "x ")
-            "x%20%20"     (encode-spaces "x  ")
-            "%20y"        (encode-spaces " y")
-            "%20%20y"     (encode-spaces "  y")
-            "x%20y"       (encode-spaces "x y")
-            "x%20y%20z"   (encode-spaces "x y z")
-            "x%20%20%20z" (encode-spaces "x   z"))))
+  "Check the jenkins-api/encode-spaces function"
+  (testing "the jenkins-api/encode-spaces function"
+           (are [x y] (= x y)
+                ""            (encode-spaces "")
+                "%20"         (encode-spaces " ")
+                "%20%20"      (encode-spaces "  ")
+                "%20%20%20"   (encode-spaces "   ")
+                "x%20"        (encode-spaces "x ")
+                "x%20%20"     (encode-spaces "x  ")
+                "%20y"        (encode-spaces " y")
+                "%20%20y"     (encode-spaces "  y")
+                "x%20y"       (encode-spaces "x y")
+                "x%20y%20z"   (encode-spaces "x y z")
+                "x%20%20%20z" (encode-spaces "x   z"))))
 
 (deftest test-encode-spaces-NPE
-    "Check the function the jenkins-api/encode-spaces"
-    (testing "the function jenkins-api/encode-spaces"
-        (is (thrown? NullPointerException (encode-spaces nil)))))
+  "Check the function the jenkins-api/encode-spaces"
+  (testing "the function jenkins-api/encode-spaces"
+           (is (thrown? NullPointerException (encode-spaces nil)))))
 
 (deftest test-job-name->url-1
-    "Check the clj-jenkins-api.jenkins-api/job-name->url"
-    (testing "the clj-jenkins-api.jenkins-api/job-name->url"
-        (are [x y] (= x y)
-            "job/"                  (job-name->url "" "job/" "")
-            " job/"                 (job-name->url " " "job/" "")
-            "jenkins-url/job/"      (job-name->url "jenkins-url/" "job/" "")
-            "jenkins-url:8080/job/" (job-name->url "jenkins-url:8080/" "job/" ""))))
+  "Check the clj-jenkins-api.jenkins-api/job-name->url"
+  (testing "the clj-jenkins-api.jenkins-api/job-name->url"
+           (are [x y] (= x y)
+                "job/"                  (job-name->url "" "job/" "")
+                " job/"                 (job-name->url " " "job/" "")
+                "jenkins-url/job/"      (job-name->url "jenkins-url/" "job/" "")
+                "jenkins-url:8080/job/" (job-name->url "jenkins-url:8080/" "job/" ""))))
 
 (deftest test-job-name->url-2
-    "Check the clj-jenkins-api.jenkins-api/job-name->url"
-    (testing "the clj-jenkins-api.jenkins-api/job-name->url"
-        (are [x y] (= x y)
-            "jenkins-url:8080/job/"                  (job-name->url "jenkins-url:8080/" "job/" "")
-            "jenkins-url:8080/job/job-name"          (job-name->url "jenkins-url:8080/" "job/" "job-name")
-            "jenkins-url:8080/job/job%20name"        (job-name->url "jenkins-url:8080/" "job/" "job name")
-            "jenkins-url:8080/job/long%20job%20name" (job-name->url "jenkins-url:8080/" "job/" "long job name"))))
+  "Check the clj-jenkins-api.jenkins-api/job-name->url"
+  (testing "the clj-jenkins-api.jenkins-api/job-name->url"
+    (are [x y] (= x y)
+         "jenkins-url:8080/job/"                  (job-name->url "jenkins-url:8080/" "job/" "")
+         "jenkins-url:8080/job/job-name"          (job-name->url "jenkins-url:8080/" "job/" "job-name")
+         "jenkins-url:8080/job/job%20name"        (job-name->url "jenkins-url:8080/" "job/" "job name")
+         "jenkins-url:8080/job/long%20job%20name" (job-name->url "jenkins-url:8080/" "job/" "long job name"))))
 
 (deftest test-job-name->url-not-NPE
-    "Check the clj-jenkins-api.jenkins-api/job-name->url"
-    (testing "the clj-jenkins-api.jenkins-api/job-name->url"
-        (are [x y] (= x y)
-            "job/"  (job-name->url "" "job/"  "")
-            "job/"  (job-name->url nil "job/" ""))))
+  "Check the clj-jenkins-api.jenkins-api/job-name->url"
+  (testing "the clj-jenkins-api.jenkins-api/job-name->url"
+           (are [x y] (= x y)
+                "job/" (job-name->url "" "job/"  "")
+                "job/" (job-name->url nil "job/" ""))))
 
 (deftest test-job-name->url-NPE
-    "Check the clj-jenkins-api.jenkins-api/job-name->url"
-    (testing "the clj-jenkins-api.jenkins-api/job-name->url"
-        (is (thrown? NullPointerException (job-name->url ""  nil nil)))
-        (is (thrown? NullPointerException (job-name->url nil nil nil)))))
+  "Check the clj-jenkins-api.jenkins-api/job-name->url"
+  (testing "the clj-jenkins-api.jenkins-api/job-name->url"
+           (is (thrown? NullPointerException (job-name->url ""  nil nil)))
+           (is (thrown? NullPointerException (job-name->url nil nil nil)))))
 
